e4bab30bf5e2ab60758d15b05ad365877fb1443f
Ana <anamaria452dd@gmail.com>
2025-02-18 18:16:15 -0300

    finalizado

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,23 +1,28 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = []
+let amigosSorteados = []
 
-function adicionarAmigo(){
-        
+function adicionarAmigo(){      
     validarEntrada()
-    console.log(amigos);
+    //console.log(amigos);
     limparNome()
 }
 
-
+//Sorteia amigo
 function sortearAmigo(){
-    let quantidadeNomes = amigos.length;
-    //Sortear amigos
+    if (amigos.length == "0"){
+        alert("A lista esta vazia, nao consigo sortear!!!")
+    }else{
+        let quantidadeNomes = amigos.length;
+        let numeroSorteado = parseInt(Math.random()* quantidadeNomes +1);
+        let nomeSorteado = amigos[numeroSorteado -1];
+        console.log(nomeSorteado);
+        //console.log(numeroSorteado)
+        amigosSorteados.push(nomeSorteado);
+        exibirResultado();
 
-    let numeroSorteado = parseInt(Math.random()* quantidadeNomes +1);
-    //numeroSorteado
-    let nomeSorteado = amigos[numeroSorteado -1];
-    console.log(nomeSorteado);
-    //console.log(numeroSorteado)
+    }
+    
 
 }
 
@@ -27,6 +32,34 @@ function validarEntrada(){
         alert("Por favor, insira um nome")
     }else{
         amigos.push(nome)
+        exibirLista()
+    }
+}
+
+function exibirLista(){
+
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i =0; i< amigos.length; i++){
+        let itemLista = document.createElement("li");
+        itemLista.textContent = amigos[i];
+        //console.log(itemLista);
+        //Incorporando a tag na lista
+        lista.appendChild(itemLista);
+
+    }
+   
+}
+
+function exibirResultado(){
+    let listaNomes = document.getElementById("resultado");
+    listaNomes.innerHTML = "";
+
+    for (let i =0; i < amigosSorteados.length;i++){
+        let itemListaNomesSorteados = document.createElement("li");
+        itemListaNomesSorteados.textContent = amigosSorteados[i];
+
+        listaNomes.appendChild(itemListaNomesSorteados);
     }
 }
 
