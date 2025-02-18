016825a77c7c8fa28bc47437a1349c67c22e6632
Ana <anamaria452dd@gmail.com>
2025-02-18 15:37:28 -0300

    segunda alteraçao

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,35 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo(){
+        
+    validarEntrada()
+    console.log(amigos);
+    limparNome()
+}
+
+
+function sortearAmigo(){
+    let quantidadeNomes = amigos.length;
+    //Sortear amigos
+
+    let numeroSorteado = parseInt(Math.random()* quantidadeNomes +1);
+    //numeroSorteado
+    let nomeSorteado = amigos[numeroSorteado -1];
+    console.log(nomeSorteado);
+    //console.log(numeroSorteado)
+
+}
+
+function validarEntrada(){
+    let nome = document.getElementById("amigo").value;
+    if (nome ==""){
+        alert("Por favor, insira um nome")
+    }else{
+        amigos.push(nome)
+    }
+}
+
+function limparNome(){
+    return document.getElementById("amigo").value = "";
+}
